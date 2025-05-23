c6a9c1833a145deba1d59210831ac228b500032a
413730663 <413730663@o365.tku.edu.tw>
2025-05-23 10:33:06 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 let video;
 let facemesh;
 let predictions = [];
-// 嘴巴外圈點位，線條會明顯顯示在嘴巴
-const indices = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95, 185, 40, 39, 37, 0, 267, 269, 270, 409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78];
+// 指定要連線的點位
+const indices = [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291];
 
 function setup() {
   createCanvas(640, 480).position(
@@ -36,13 +36,14 @@ function draw() {
     stroke(255, 0, 0);
     strokeWeight(15);
     noFill();
-    beginShape();
-    for (let i = 0; i < indices.length; i++) {
-      const idx = indices[i];
-      const [x, y] = keypoints[idx];
-      vertex(x, y);
+    // 用 line 指令將所有點串接
+    for (let i = 0; i < indices.length - 1; i++) {
+      const idxA = indices[i];
+      const idxB = indices[i + 1];
+      const [x1, y1] = keypoints[idxA];
+      const [x2, y2] = keypoints[idxB];
+      line(x1, y1, x2, y2);
     }
-    endShape(CLOSE);
   }
   pop();
 }
